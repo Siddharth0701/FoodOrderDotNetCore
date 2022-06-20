6b1da789de91b40f4b9192cfdf28656840a4b2d2
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-06-20 19:05:47 +0530

    Added model as category and applyed the Migration

---
+++ FoodOrderDotNetCore/Areas/Admin/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using FoodOrderDotNetCore.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOrderDotNetCore.Areas.Admin
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- FoodOrderDotNetCore/Data/ApplicationDbContext.cs
+++ FoodOrderDotNetCore/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FoodOrderDotNetCore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,6 @@ namespace FoodOrderDotNetCore.Data
             : base(options)
         {
         }
+        public DbSet<Category> Category{ get; set; }
     }
 }
