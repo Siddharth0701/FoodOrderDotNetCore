5894b9cb59cfbe594975e6f7a5033e719e1d16a0
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-06-21 22:46:58 +0530

    Added partial view and view for create,edit

--- FoodOrderDotNetCore/Areas/Admin/Controllers/CategoryController.cs
+++ FoodOrderDotNetCore/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using FoodOrderDotNetCore.Data;
+using FoodOrderDotNetCore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -22,5 +23,53 @@ namespace FoodOrderDotNetCore.Areas.Admin.Controllers
 
             return View(await _db.Category.ToListAsync());
         }
+        //Get create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Post -Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                //if valid
+                _db.Category.Add(category);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(category);
+        }
+
+        //Get -Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(category);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
     }
 }
