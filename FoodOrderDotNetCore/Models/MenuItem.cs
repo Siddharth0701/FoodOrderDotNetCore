3f18bb23b43c63756498bfca0d609d402d330b86
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-06-23 18:14:23 +0530

    Added create,delete in sub category and menu item as a new model

---
+++ FoodOrderDotNetCore/Models/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOrderDotNetCore.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
